4bf0c1a1e9cf57789a5af5382f824995c96e95d2
Sebas17b8 <sayala98@hotmail.com>
2020-06-11 18:43:53 -0500

    Se agregaron nuevas interfaces, aun no probadas en mongo

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var Estrella = require('../model/estrella.js');
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.get('/alta', (req, res, next)=>{
+  res.render('alta_estrella', {})
+});
 
+router.get('/lista', (req, res, next)=>{
+  Estrella.find({}, (err, data)=>{
+    console.log(data);
+    if(err) res.send("Error: " + err);
+    else res.render('catalogo', {comic:data});
+  });
+});
+
+router.get('/buscar', (req, res, next)=>{
+  res.render('busqueda', {})
+});
 module.exports = router;
